8b0645302cb78a1a6467ba3430eaeddff8933e3b
desktop <138785922+ptyytty@users.noreply.github.com>
2024-08-11 07:28:18 +0900

    need DB

--- Reminder/Front/script/join.js
+++ Reminder/Front/script/join.js
@@ -1,6 +1,17 @@
 const join = document.getElementById("join");
 
-
 function joinLink(){
     location.href = "main.html";
+}
+
+function submit(){
+    const id = document.getElementById("id").value;
+    const pws = document.getElementById("pws").value;
+
+    if(id == "" || pws == ""){
+        alert("회원가입을 할 수 없습니다.")
+    }else{
+        alert("회원가입 성공\n환영합니다!")
+        location.replace('mynote.html');
+    }
 }
